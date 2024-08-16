9642694bb7e1bf4463f3e8391503688835beaa61
Javier Rocha Gómez <javierrg1992@gmail.com>
2024-08-16 19:39:14 +0200

    text extraction from image using react-native-tesseractocr

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React, { useState } from 'react';
-import { View, Button, Image, StyleSheet, Alert } from 'react-native';
+import { View, Button, Image, StyleSheet, Alert, Text } from 'react-native';
 import DocumentScanner from 'react-native-document-scanner-plugin';
+import TesseractOcr, { useEventListener } from '@devinikhiya/react-native-tesseractocr';
 
 export default function App() {
   const [scannedImage, setScannedImage] = useState(null);
   const [scanning, setScanning] = useState(false);
+  const [resultText, setResultText] = useState(null);
 
   const scanDocument = async () => {
     try {
       // Start the document scanner
       const { scannedImages } = await DocumentScanner.scanDocument();
-      
+
       // Get back an array with scanned image file paths
       if (scannedImages.length > 0) {
-        // Set the img src, so we can view the first scanned image
-        setScannedImage(scannedImages[0]);
+        const imageUri = scannedImages[0];
+        setScannedImage(imageUri);
+
+        try {
+          // Download langujae packages from https://github.com/tesseract-ocr/tessdata
+          const recognizedText = await TesseractOcr.recognize(
+            imageUri,
+            'spa',
+            {},
+          );
+          console.log('ESTE ES EL TIKET DE LA COMPRA');
+          console.log(recognizedText)
+        } catch (error) {
+          console.log('error tesseract is', error);
+        }
+
+
       }
     } catch (error) {
       console.error('Error scanning document:', error);
@@ -23,6 +40,8 @@ export default function App() {
       setScanning(false); // End scanning mode
     }
   };
+
+
 
   return (
     <View style={styles.container}>
@@ -37,11 +56,14 @@ export default function App() {
           />
         </View>
       ) : (
-        <Image
-          resizeMode="contain"
-          style={styles.image}
-          source={{ uri: scannedImage }}
-        />
+        <>
+          <Image
+            resizeMode="contain"
+            style={styles.image}
+            source={{ uri: scannedImage }}
+          />
+          {resultText && <View style={styles.resultContainer}><Text>{resultText}</Text></View>}
+        </>
       )}
     </View>
   );
@@ -59,7 +81,12 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: '80%',
+    height: 300,
+    marginVertical: 20,
+  },
+  resultContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
   },
 });
